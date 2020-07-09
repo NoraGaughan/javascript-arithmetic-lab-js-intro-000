86ca4105a3212593b9d682fd44ae0e731f6fc84f
Nora Gaughan <noragaughan@gmail.com>
2020-07-09 22:41:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var number = 10
 
-function add5() {number += 5}
+function add() {number += 5}
 
-function subtract5() {number -=5}
+function subtract() {number -=5}
 
-function multiple5() {number *= 5}
+function multiple() {number *= 5}
 
-function divide5() {number /=5}
+function divide() {number /=5}
